41ecd5084483c62548be8c4d3b81f2c9ae68a454
PC <@pc>
2024-08-28 20:59:53 +0300

    Удалены некоторые методы.

--- src/main/java/ru/plm/log/LogAPI.java
+++ src/main/java/ru/plm/log/LogAPI.java
@@ -7,17 +7,9 @@ public interface LogAPI {
 
     void info(Plugin plugin, String message);
 
-    void info(Plugin plugin, String message, Object arg1);
-
-    void info(Plugin plugin, String message, Object arg1, Object arg2);
-
     void info(Plugin plugin, String message, Object... args);
 
     void warn(Plugin plugin, String message);
-
-    void warn(Plugin plugin, String message, Object arg1);
-
-    void warn(Plugin plugin, String message, Object arg1, Object arg2);
 
     void warn(Plugin plugin, String message, Object... args);
 }
