9e3a2a066fac0543b96f63fa39555122c4e716b6
PC <@pc>
2024-08-28 22:05:19 +0300

    Добавлен метод command.

--- src/main/java/ru/plm/log/LogAPI.java
+++ src/main/java/ru/plm/log/LogAPI.java
@@ -1,9 +1,12 @@
 package ru.plm.log;
 
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 @SuppressWarnings("unused")
 public interface LogAPI {
+
+    void command(Plugin plugin, Player player, String command);
 
     void info(Plugin plugin, String message);
 
